5766f6f79f2595d1c47c5e6892bcc4054d3be369
Aleksandr Shapovalov <giiks131@gmail.com>
2021-10-25 22:33:47 +0300

    First homework 1.3
    First homework completed

---
+++ Navigation/Controllers/ProfileViewController.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+class FirstProfileViewController: UIViewController {
+    let button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = Post(title:"Profile").title
+        
+        view.addSubview(button)
+        button.setTitle(Post(title:"Show my Card").title, for: .normal)
+        button.backgroundColor = .systemMint
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButtonOne), for: .touchUpInside)
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain,target: self, action: #selector(didTapButtonTwo))
+    }
+   
+    @objc private func didTapButtonOne() {
+        let rootVC = InfoViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        present(navVC, animated: true)
+    }
+    @objc private func didTapButtonTwo() {
+        let rootVC = SecondProfileViewController()
+        rootVC.view.backgroundColor = .white
+        navigationController?.pushViewController(rootVC, animated: true)
+    }
+}
+
+class SecondProfileViewController: UIViewController {
+   
+    private let button = UIButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Settings"
+        view.addSubview(button)
+        button.setTitle(Post(title: "Show my settings").title, for: .normal)
+        button.backgroundColor = .systemPurple
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 60)
+        button.titleLabel?.textAlignment = .center
+        button.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+    }
+   
+    @objc func showAlert() {
+        let alert = UIAlertController(title: Post(title:"My settings").title, message: "Settings:", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { action in
+            print("Settings were shown")
+        }))
+        present(alert, animated: true)
+    }
+}
+
+class InfoViewController: UIViewController {
+
+    var button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+        title = Post(title: "My profile").title
+        view.addSubview(button)
+        button.setTitle(Post(title:"Show Info").title, for: .normal)
+        button.backgroundColor = .systemPurple
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 60)
+        button.titleLabel?.textAlignment = .center
+        button.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+    }
+    @objc func showAlert() {
+        let alert = UIAlertController(title: Post(title:"Info").title, message: "Sorry, no info. Please check settings", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { action in
+            print("Info was shown")
+        }))
+        present(alert, animated: true)
+    }
+}
+
+struct Post {
+    let title: String
+}
---
+++ Navigation/Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    func setupTabBar() {
+        let feedViewController = creativeNavController(viewController: FeedViewController(), itemName: "Feed", ItemImage: "newspaper")
+        let messagesViewController = creativeNavController(viewController: MessagesViewController(), itemName: "Messages", ItemImage: "message.fill")
+        let profileViewController = creativeNavController(viewController: FirstProfileViewController(), itemName: "Profile", ItemImage: "rectangle.stack.person.crop")
+        
+        viewControllers = [feedViewController, messagesViewController, profileViewController]
+
+    }
+    
+    func creativeNavController(viewController: UIViewController, itemName: String, ItemImage: String) -> UINavigationController {
+     
+        let item = UITabBarItem(title: itemName, image: UIImage(systemName: ItemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0)), tag: 0)
+        item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
+       
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem = item
+        
+        return navController
+    }
+}
+
