7fc1043185e4e05b8e4f9dd463fe89bbc18554b5
Aleksandr Shapovalov <giiks131@gmail.com>
2021-11-01 15:17:47 +0300

    Moved UI from viewDidLoad to closures
    Moved UI from viewDidLoad to closures

--- Navigation/Controllers/Profile/ProfileHeaderView.swift
+++ Navigation/Controllers/Profile/ProfileHeaderView.swift
@@ -1,22 +1,60 @@
-//
-//  ProfileHeaderView.swift
-//  Navigation
-//
-//  Created by Александр Шаповалов on 27.10.2021.
-//
-
 import UIKit
 
 class ProfileViewHeader: UIView {
-    var labelProfileName = UILabel()
     
-    var labelStatus = UILabel()
-    private var statusText: String = "Waiting for something..."
-    var avatar = UIImageView()
+    var statusText: String = "Waiting for something..."
+
+    var fullNameLabel: UILabel = {
+        let fullNameLabel = UILabel()
+        fullNameLabel.text = "Happy Cat"
+        fullNameLabel.frame = CGRect(x: 145, y: 118, width: 100, height: 21)
+        fullNameLabel.backgroundColor = .clear
+        fullNameLabel.textAlignment = .center
+        fullNameLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        fullNameLabel.textColor = .black
+        fullNameLabel.numberOfLines = 1
+        return fullNameLabel
+    }()
     
-    var placeHolder = UITextField()
+    var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.text = "Waiting for something..."
+        statusLabel.frame = CGRect(x: 150, y: 185, width: 170, height: 20)
+        statusLabel.backgroundColor = .clear
+        statusLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusLabel.textColor = .gray
+        statusLabel.numberOfLines = 1
+        statusLabel.sizeToFit()
+        return statusLabel
+    }()
     
-    var buttonone: UIButton = {
+    var avatarImageView: UIImageView = {
+        let avatar = UIImageView()
+        avatar.frame = CGRect(origin: CGPoint(x: 16, y: 107), size: CGSize(width: 130, height: 130))
+        avatar.image = UIImage(named: "catAvatar")
+        avatar.layer.borderWidth = 3
+        avatar.layer.borderColor = UIColor.white.cgColor
+        avatar.layer.cornerRadius = avatar.frame.size.width / 2
+        avatar.layer.masksToBounds = true
+        return avatar
+    }()
+    
+    var statusTextField: UITextField = {
+        let placeHolder = UITextField()
+        placeHolder.frame = CGRect(x: 150, y: 210, width: 224, height: 45)
+        placeHolder.placeholder = "Enter new status :)"
+        placeHolder.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        placeHolder.backgroundColor = .white
+        placeHolder.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 40))
+        placeHolder.leftViewMode = .always
+        placeHolder.layer.cornerRadius = 12
+        placeHolder.layer.borderColor = UIColor.black.cgColor
+        placeHolder.layer.borderWidth = 1
+        placeHolder.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
+        return placeHolder
+    }()
+    
+    var setStatusButton: UIButton = {
         var button = UIButton(frame: .zero)
         button.setTitle(Post(title:"Set new status").title, for: .normal)
         button.backgroundColor = .systemPurple
@@ -31,50 +69,14 @@ class ProfileViewHeader: UIView {
         button.addTarget(self, action: #selector(setStatus), for: .touchUpInside)
         return button
     }()
-   
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(buttonone)
-        
-        addSubview(avatar)
-        avatar.frame = CGRect(origin: CGPoint(x: 16, y: 107), size: CGSize(width: 130, height: 130))
-        avatar.image = UIImage(named: "catAvatar")
-        avatar.layer.borderWidth = 3
-        avatar.layer.borderColor = UIColor.white.cgColor
-//
-        avatar.layer.cornerRadius = avatar.frame.size.width / 2
-        avatar.layer.masksToBounds = true
-        
-        addSubview(labelProfileName)
-        labelProfileName.text = "Happy Cat"
-        labelProfileName.frame = CGRect(x: 145, y: 118, width: 100, height: 21)
-        
-        labelProfileName.backgroundColor = .clear
-        labelProfileName.textAlignment = .center
-        labelProfileName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        labelProfileName.textColor = .black
-        labelProfileName.numberOfLines = 1
-        
-        addSubview(labelStatus)
-        labelStatus.text = statusText
-        labelStatus.frame = CGRect(x: 150, y: 185, width: 170, height: 20)
-        labelStatus.backgroundColor = .clear
-        labelStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        labelStatus.textColor = .gray
-        labelStatus.numberOfLines = 1
-        labelStatus.sizeToFit()
-        
-        addSubview(placeHolder)
-        placeHolder.frame = CGRect(x: 150, y: 210, width: 224, height: 45)
-        placeHolder.placeholder = "Enter new status :)"
-        placeHolder.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        placeHolder.backgroundColor = .white
-        placeHolder.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 40))
-        placeHolder.leftViewMode = .always
-        placeHolder.layer.cornerRadius = 12
-        placeHolder.layer.borderColor = UIColor.black.cgColor
-        placeHolder.layer.borderWidth = 1
-        placeHolder.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
+        addSubview(setStatusButton)
+        addSubview(avatarImageView)
+        addSubview(fullNameLabel)
+        addSubview(statusLabel)
+        addSubview(statusTextField)
     }
    
     required init?(coder: NSCoder) {
@@ -82,12 +84,11 @@ class ProfileViewHeader: UIView {
     }
     
     @objc func setStatus() {
-        labelStatus.text = String(statusText)
-        print("Status: \(labelStatus.text ?? "nil")")
+        statusLabel.text = String(statusText)
+        print("Status: \(statusLabel.text ?? "nil")")
     }
     
     @objc func statusTextChanged(_ textField: UITextField) {
-        statusText = placeHolder.text ?? "Empty"
+        statusText = statusTextField.text ?? "Empty"
     }
 }
-
